c30b8de8b946fdfabb29191a3f87ebab126bd744
heiba77-7 <157486162+heiba77-7@users.noreply.github.com>
2024-10-08 21:49:55 +0300

    Delete Crm directory

--- Crm/Models/CustomerModels/CustomerContact.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-public class CustomerContact{
-    public int Id {set;get;}
-    public int CustomerId{set;get;}
-    public string? Contact{set;get;} 
-    [JsonIgnore]
-    public Customer? Customer{set; get;}
-}
