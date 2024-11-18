81926d3d6b5ab340bbdf6fffcdc77e8ac2a4a4d6
heiba77-7 <mohamedheiba2022@gmail.com>
2024-11-18 19:51:18 +0200

    after adding style and scss

--- Frontend/src/Pages/AppLayOut.jsx
+++ Frontend/src/Pages/AppLayOut.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from 'react-router-dom'
+import TextControlsExample from '../Components/TextControlsExample'
 
 export default function AppLayOut() {
     return (
         <div className="App">
             <div className="App-header">
+                <TextControlsExample/>
                 <Outlet/>
             </div>
         </div>
